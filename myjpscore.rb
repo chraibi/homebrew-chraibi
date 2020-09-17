7f3cf1492c9303152df62c6ad614946402bf0710
Mohcine Chraibi <m.chraibi@gmail.com>
2020-09-17 08:48:33 +0200

    Update myjpscore.rb

--- myjpscore.rb
+++ myjpscore.rb
@@ -40,7 +40,7 @@ class Myjpscore < Formula
   test do
     last_release="0.8.4"
     #assert_match version.to_s, shell_output("jpsreport -v 2>/dev/null")
-    test_version=shell_output("./bin/jpscore 2>/dev/null | grep Version | awk  '{ print $5 }' |  tr -d '[[:space:]]'")
+    test_version=shell_output("jpscore 2>/dev/null | grep Version | awk  '{ print $5 }' |  tr -d '[[:space:]]'")
     ohai "checking version:"
     ohai "- expected: <#{last_release}>"
     ohai "- got: <#{test_version}>"
